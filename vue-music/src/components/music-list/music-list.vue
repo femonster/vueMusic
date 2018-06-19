a7eefc6a8cd43a7c02dd43ae8019273288534c5e
gengkai <409465662@qq.com>
2018-06-19 23:15:49 +0800

    .

---
+++ vue-music/src/components/music-list/music-list.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="music-list"></div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="stylus" scoped>
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+
+.music-list
+    position: fixed
+    z-index: 100
+    top: 0
+    left: 0
+    bottom: 0
+    right: 0
+    background: $color-background
+</style>
+
+
