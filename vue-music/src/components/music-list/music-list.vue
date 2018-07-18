dd757d9f3420092fc08117faf1dd78dd4a46e80a
gengkai <409465662@qq.com>
2018-07-18 09:49:57 +0800

    diao

--- vue-music/src/components/music-list/music-list.vue
+++ vue-music/src/components/music-list/music-list.vue
@@ -11,10 +11,33 @@
                     <span class="text">随机播放全部</span>
                 </div>
             </div>
+            <div class="filter" ref="filter"></div>
         </div>
+        <div class="bg-layer" ref="layer"></div>
+        <scroll 
+        :data="songs"
+        @scroll="scroll"
+        :listen-scroll="listenScroll"
+        :probe-type="probeType"
+        class="list" 
+        ref="list"
+        >
+            <div class="song-list-wrapper">
+                <song-list :songs="songs"></song-list>
+            </div>
+            <div v-show="!songs.length" class="loading-container">
+                <loading></loading>
+            </div>
+        </scroll>
     </div>
 </template>
 <script>
+import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
+import songList from 'base/song-list/song-list'
+
+const RESERVED_HEIGHT = 40
+
 export default {
     props:{
         bgImage:{
@@ -30,6 +53,20 @@ export default {
             default:""
         }    
     },
+    data(){
+        return {
+            scrollY: 0
+        }
+    },
+    created(){
+        this.probeType = 3
+        this.listenScroll = true
+    },
+    mounted(){
+        this.imageHeight = this.$refs.bgImage.clientHeight
+        this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+        this.$refs.list.$el.style.top = `${this.imageHeight}px`
+    },
     computed:{
         bgStyle(){
             return `background-image:url(${this.bgImage})`
@@ -38,7 +75,36 @@ export default {
     methods:{
         back() {
             this.$router.back()
+        },
+        scroll(pos) {
+            this.scrollY = pos.y
         }
+    },
+    watch:{
+        scrollY(newVal){
+            let translateY = Math.max(this.minTranslateY,newVal)
+            let zIndex = 0
+            this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+            this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+
+            if(newVal<this.minTranslateY){
+                // 当到边界时
+                zIndex = 10
+                this.$refs.bgImage.style.paddingTop = 0
+                this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+                this.$refs.playBtn.style.display = 'none'
+            }else{
+                this.$refs.bgImage.style.paddingTop = '70%'
+                this.$refs.bgImage.style.height = 0
+                this.$refs.playBtn.style.display = ''
+            }
+            this.$refs.bgImage.style.zIndex = zIndex
+        }
+    },
+    components:{
+        Scroll,
+        songList,
+        Loading
     }
 }
 </script>
@@ -106,7 +172,30 @@ export default {
                     display inline-block
                     vertical-align middle
                     font-size $font-size-small 
-
+        .filter
+            position: absolute
+            top: 0
+            left: 0
+            width: 100%
+            height: 100%
+            background: rgba(7, 17, 27, 0.4)
+    .bg-layer
+        position: relative
+        height: 100%
+        background: $color-background
+    .list 
+        position fixed
+        top 0
+        bottom 0
+        width 100%
+        background-color $color-background
+        .song-list-wrapper
+            padding: 20px 30px
+        .loading-container
+            position: absolute
+            width: 100%
+            top: 50%
+            transform: translateY(-50%)
 </style>
 
 
